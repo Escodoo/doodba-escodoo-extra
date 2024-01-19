9920200424579e4416df70b717ea466a37cbedc9
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-01-19 16:37:14 -0300

    Adicionado update de escodoo.py, automação da instalação dao requests

--- escodoo.py
+++ escodoo.py
@@ -1,15 +1,14 @@
 """Escodoo child project tasks.
 
 This file is to be executed with https://www.pyinvoke.org/ in Python 3.6+.
-
 Contains common helpers to develop using this child project.
 """
 import os
 import subprocess
 import sys
+from datetime import datetime
 from logging import getLogger
 from pathlib import Path
-from datetime import datetime
 
 from invoke import exceptions, task
 
@@ -22,25 +21,23 @@ _logger = getLogger(__name__)
 
 
 def get_invoke_venv_path():
-    """Obtém o caminho do ambiente virtual do invoke gerenciado pelo pipx."""
+    """Gets the path of invoke's virtual environment managed by pipx."""
     try:
         result = subprocess.run(
             ["pipx", "environment", "--value", "PIPX_LOCAL_VENVS"],
-            capture_output=True,
-            text=True,
-            check=True
+            capture_output=True, text=True, check=True
         )
         pipx_venv_root = Path(result.stdout.strip())
         invoke_venv_path = pipx_venv_root / "invoke" / "bin"
         return invoke_venv_path
     except subprocess.CalledProcessError:
-        print("Não foi possível obter o caminho do ambiente virtual do invoke.")
+        print("Could not obtain the path of invoke's virtual environment.")
         return None
 
 
 def install_package_in_venv(venv_path, package):
-    """Instala um pacote no ambiente virtual especificado."""
-    python_executable = venv_path / "python"  # Caminho para o Python no ambiente virtual do invoke
+    """Installs a package in the specified virtual environment."""
+    python_executable = venv_path / "python"
     try:
         subprocess.check_call([python_executable, "-m", "pip", "install", package])
     except subprocess.CalledProcessError:
@@ -54,7 +51,7 @@ except ImportError:
     invoke_venv_path = get_invoke_venv_path()
     if invoke_venv_path and invoke_venv_path.exists():
         install_package_in_venv(invoke_venv_path, "requests")
-        import requests  # Tenta importar novamente após a instalação
+        import requests  # Tries to import again after installation
     else:
         print("Invoke virtual environment path not found.")
         sys.exit(1)
@@ -65,17 +62,11 @@ UID_ENV = {
     "GID": os.environ.get("DOODBA_GID", str(os.getgid())),
     "UID": os.environ.get("DOODBA_UID", str(os.getuid())),
     "DOODBA_UMASK": os.environ.get("DOODBA_UMASK", "27"),
+    "DOODBA_GITAGGREGATE_GID": os.environ.get("DOODBA_GITAGGREGATE_GID", ""),
+    "DOODBA_GITAGGREGATE_UID": os.environ.get("DOODBA_GITAGGREGATE_UID", ""),
 }
-UID_ENV.update(
-    {
-        "DOODBA_GITAGGREGATE_GID": os.environ.get(
-            "DOODBA_GITAGGREGATE_GID", UID_ENV["GID"]
-        ),
-        "DOODBA_GITAGGREGATE_UID": os.environ.get(
-            "DOODBA_GITAGGREGATE_UID", UID_ENV["UID"]
-        ),
-    }
-)
+UID_ENV["DOODBA_GITAGGREGATE_GID"] = UID_ENV.get("DOODBA_GITAGGREGATE_GID", UID_ENV["GID"])
+UID_ENV["DOODBA_GITAGGREGATE_UID"] = UID_ENV.get("DOODBA_GITAGGREGATE_UID", UID_ENV["UID"])
 SERVICES_WAIT_TIME = int(os.environ.get("SERVICES_WAIT_TIME", 4))
 ODOO_VERSION = float(
     yaml.safe_load((PROJECT_ROOT / "common.yaml").read_text())["services"]["odoo"][
@@ -84,17 +75,16 @@ ODOO_VERSION = float(
 )
 
 
-# Função para transformar a URL do GitHub para a URL do conteúdo bruto
 def transform_github_url_to_raw_url(github_url, branch, path):
+    """Transforms a GitHub URL to a raw content URL."""
     parts = github_url.split("/")
-    user_or_org = parts[3]
-    repo = parts[4]
+    user_or_org, repo = parts[3], parts[4]
     raw_url = f"https://raw.githubusercontent.com/{user_or_org}/{repo}/{branch}/{path}"
     return raw_url
 
 
-# Função para baixar o conteúdo de uma URL
 def download_file(url):
+    """Downloads content from a URL."""
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -105,8 +95,7 @@ def download_file(url):
 
 @task(help={"github_url": "URL of the GitHub repository"})
 def overwrite_project_files(c, github_url):
-    """Overwrite local files by template remote files (spec.yaml, repos.yaml, apt.txt, gem.txt, npm.txt, pip.txt if exist in repo)
-    """
+    """Overwrites local files with template remote files."""
     odoo_version = str(int(ODOO_VERSION))
     files_to_update = {
         "repos.yaml": "odoo/custom/src/repos.yaml",
@@ -116,120 +105,85 @@ def overwrite_project_files(c, github_url):
         "npm.txt": "odoo/custom/dependencies/npm.txt",
         "pip.txt": "odoo/custom/dependencies/pip.txt",
     }
-
     for file_name, file_path in files_to_update.items():
         url = transform_github_url_to_raw_url(github_url, f"{odoo_version}.0", file_path)
         content = download_file(url)
-        if content is not None:
+        if content:
             local_path = PROJECT_ROOT / file_path
             with open(local_path, 'w') as file:
                 file.write(content)
             print(f"File updated: {file_name}")
 
-    print("Update Escodoo config files is done!")
+    print("Escodoo config files update completed.")
 
 
 @task(help={
-    "dbname": "The DB if exits that will be DESTROYED and recreated. Default: 'devel'.",
-    "demo": "Use --demo to force install demo data.",
-    "no_demo": "Use --no-demo to force exclude demo data.",
-    "extra_modules": "Comma-separated list of extra modules to be installed in the second step (e.g., 'module1,module2,module3'). Default: ''",
-    "language": "Language code to force the language for the installation (e.g., 'pt_BR', 'en_US'). If not set, defaults to 'en_US' or the language configured in odoo.conf.",
+    "dbname": "Database name to be DESTROYED and recreated. Default: 'devel'.",
+    "extra_modules": "Extra modules to install (comma-separated). Default: ''",
+    "language": "Language code for database initialization (e.g., 'pt_BR', 'en_US'). "
+                "Defaults to the system's Odoo language, typically English, "
+                "or to the 'load_language' parameter in odoo.conf, if specified.",
+    "demo": "Use --demo to install demo data. "
+            "This will overwrite the environment's demo data definition.",
+    "no_demo": "Use --no-demo to exclude demo data. "
+               "This will overwrite the environment's demo data definition.",
 })
-def preparedb_escodoo(c, dbname="devel", demo=False, no_demo=False, extra_modules='', language=None):
+def preparedb_escodoo(c, dbname="devel", demo=False, no_demo=False,
+                      extra_modules='', language=None):
     """
-    Prepare the Escodoo database with a specific profile in two steps.
+    Prepares the Escodoo database in two steps.
 
-    First, this script sets up an Odoo database for Escodoo by installing
-    escodoo_setup_base with an optional specific language. Then, it installs
-    escodoo_setup_base_br and any extra modules provided. It uses docker-compose
-    to manage Odoo containers and performs database operations such as dropping
-    and installing databases.
-
-    Note:
-        Make sure to test the script in a safe environment before applying it to production.
+    Initially, sets up an Odoo database for Escodoo by installing
+    escodoo_setup_base with a specified language. Then, installs
+    escodoo_setup_base_br and any additional modules provided.
+    Utilizes docker-compose for managing Odoo containers and
+    performs database operations like dropping and installing databases.
     """
-    # Checks for Odoo version compatibility
     if ODOO_VERSION < 11:
         raise exceptions.PlatformError(
-            "The preparedb_escodoo script is not available for Doodba environments below v11."
+            "Preparedb_escodoo script not available for Doodba below v11."
         )
-
-    # Stops the Odoo service to prevent conflicts
     c.run("docker-compose stop odoo", pty=True)
     _run = "docker-compose run --rm -l traefik.enable=false odoo"
 
     with c.cd(str(PROJECT_ROOT)):
-        # Determines the demo data and language parameter based on 'demo' and 'language'
-        demo_param = "--without-demo=False" if demo else ""
-        demo_param = "--without-demo=ALL" if no_demo else ""
-        language_param = f"--load-language={language}" if language else "--load-language=en_US"
+        demo_param = "--without-demo=ALL" if no_demo else \
+                     ("--without-demo=False" if demo else "")
+        language_param = f"--load-language={language}" \
+                         if language else ""
 
-        # Drops existing database if requested
-        c.run(
-            f"{_run} click-odoo-dropdb {dbname}",
-            env=UID_ENV,
-            warn=True,
-            pty=True,
-        )
+        c.run(f"{_run} click-odoo-dropdb {dbname}", env=UID_ENV, warn=True, pty=True)
+        c.run(f"{_run} -d {dbname} -i escodoo_setup_base {demo_param} "
+              f"{language_param} --stop-after-init", env=UID_ENV, pty=True)
 
-        # First step: Create a new database and install the escodoo_setup_base module with the specified language
-        c.run(
-            f"{_run} -d {dbname} -i escodoo_setup_base {demo_param} {language_param} --stop-after-init",
-            env=UID_ENV,
-            pty=True,
-        )
-
-        # Formats the extra modules to be included in the second installation command
         extra_modules_param = ',' + extra_modules if extra_modules else ''
-
-        # Second step: Install escodoo_setup_base_br and any extra modules
-        c.run(
-            f"{_run} -d {dbname} -i escodoo_setup_base_br{extra_modules_param} --stop-after-init",
-            env=UID_ENV,
-            pty=True,
-        )
-
-    # TODO:
-    # - Implementar forma de atualizar o arquivo escodoo.py
-    # - Avaliar se o melhor é criar as bases com o click-odoo-createdb passando como parametro o odoo.conf para garantir a seleção da linguagem
-
-    # Observações:
-    # A justificativa de ter o modulo escodoo_setup_base é para que seja um padrão independente de que pais vamos implantar
-    # o Odoo. Então teremos uma versão base para qq pais e um modulo como o escodoo_setup_base_br para o Brasil e podemos ter outros
-    # modulos como escodoo_setup_base_ar para a Argentina, escodoo_setup_base_mx para o México, etc.
+        c.run(f"{_run} -d {dbname} -i escodoo_setup_base_br{extra_modules_param} "
+              "--stop-after-init", env=UID_ENV, pty=True)
 
 
-@task(help={"branch": "Branch of the GitHub repository to download the script from. Default: 'main'."})
+@task(help={"branch": "GitHub repository branch. Default: 'main'."})
 def update_script(c, branch="main"):
     """
-    Update the escodoo.py script with the latest version from the GitHub repository.
-    Saves a backup copy of the current script with a timestamp before updating.
+    Updates the escodoo.py script to the latest version from the GitHub repository.
+    Saves a backup copy of the current script with a timestamp.
     """
     script_url = transform_github_url_to_raw_url(
-        "https://github.com/Escodoo/doodba-escodoo-extra",
-        branch,
-        "escodoo.py"
+        "https://github.com/Escodoo/doodba-escodoo-extra", branch, "escodoo.py"
     )
     new_script_content = download_file(script_url)
-    if new_script_content is not None:
+    if new_script_content:
         script_path = PROJECT_ROOT / "escodoo.py"
-
-        # Save a backup of the current script with timestamp
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
         backup_script_path = PROJECT_ROOT / f"escodoo.py.{timestamp}"
         os.rename(script_path, backup_script_path)
-        print(f"Backup of the current script saved as: {backup_script_path}")
+        print(f"Backup saved as: {backup_script_path}")
 
-        # Update the script with the new content
         with open(script_path, 'w') as file:
             file.write(new_script_content)
-        print("The escodoo.py script has been updated to the latest version.")
+        print("escodoo.py script updated.")
 
-        # Instructions to delete the backup file
-        print("\nTo delete the backup file, use one of the following commands:")
+        print("\nTo delete the backup file:")
         print(f"Linux/Mac: rm {backup_script_path}")
         print(f"Windows: del {backup_script_path}")
     else:
-        print("Failed to update the escodoo.py script. Please check the URL or your internet connection.")
-
+        print("Failed to update escodoo.py. Check URL or internet connection.")
